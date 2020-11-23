205bc33dd7e9c85556b6c845d5402bb44689b9a1
Shikhar Gupta <shikhar.gupta@utexas.edu>
2020-11-23 14:11:33 -0600

    added tests

--- scripts/verify_table.sh
+++ scripts/verify_table.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #***************************************************************************************#
 #										        #
-# FILE: verify_table.sh								        #
+# FILE: verify_table.sh							        #
 #										        #
-# USAGE: verify_table.sh [-h] [-d <database_name>] -t <table_name> -f <path>  		#
+# USAGE: verify_table.sh [-h] [-d <database_name>] -t <table_name> -f <path>  #
 #										        #
 # DESCRIPTION: Verifies that a PostgreSQL table imported all data from a CSV file.	#
 #											#
@@ -12,8 +12,8 @@
 # ERROR CONDITIONS: exit 1 ---- Invalid option						#
 #                   exit 2 ----	PostgreSQL not installed		              	#
 #                   exit 3 ----	Invalid file or folder given				#
-#                   exit 4 ----	Error in psql						#
-#                   exit 5 ----	Error in parsing csv					#
+#                   exit 4 ----	Error in psql		#
+#                   exit 5 ----	Error in parsing csv				#
 #                   exit 99 ---	killed by external forces				#
 #											#
 # DEVELOPER: Shikhar Gupta								#
@@ -27,13 +27,14 @@
 # Define the shell functions
 usage(){
 	echo "
-	Usage: $0 [-h] [-d <database_name>] -t <table_name> -f <path>
+	Usage: $0 [-h] [-d <database_name>] [k <number>] -t <table_name> -f <path>
 
 	Options:
 		-h 	Display help message.
 		-d	Specify a database name (Default is \"test\").
                 -t      Specify a table name (Required).
 		-f	Specify a csv filename (Required).
+		-k  	Specify number of keys (or non-data columns, default is 0).
 	" >&2
 	exit 0
 }
@@ -55,11 +56,12 @@ fi
 fname=""
 database="test"
 tablename=""
-tests=1
+tests=3
 passed=0
+keys=0
 
 # Parse command line arguments
-while getopts "hf:d:t:" opt; do
+while getopts "hf:d:t:k:" opt; do
 	  case $opt in
 	          h) usage
 			 ;;
@@ -69,10 +71,18 @@ while getopts "hf:d:t:" opt; do
 			 ;;
 			  t) tablename=$OPTARG
 			 ;;
-		 \?) die "Error---->Invalid option: -$OPTARG" 1
+			  k) keys=$OPTARG
+			 ;;
+		 \?) die "Error ----> Invalid option: $OPTARG" 1
 		     ;;
 	  esac
 done
+
+# Verify keys is a numerical value (From https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash)
+re='^[0-9]+$'
+if ! [[ $keys =~ $re ]] ; then
+   die "Error ----> Must specify a numerical value for k" 1
+fi
 
 # Verify psql installation exists
 psql --version
@@ -92,34 +102,81 @@ else
 	die "Error----> Please enter a valid .csv file name." 3
 fi
 
-# Test One: Check number of rows in csv and psql table
-echo
-echo "*** Test One: Number of Rows ***"
+# Test One: Check csv is formatted correctly (num commas is the same in every row)
+echo "*** Test One: CSV Formatting ***"
 
-# Get rows in table
+# Get num rows in the csv (-1 for the header row)
+csvlines=$(($(cat $fname | wc -l) - 1))
+if [[ $? != 0 ]]; then
+	echo "** FAILED **"
+	die "Error in csv file." 5
+fi
+
+# Returns num commas in the first row + 1 (For newline) and returns count (num commas + 1 = num cols)
+csvcols=$(($(cat $fname | head -1 $fname | sed 's/[^,]//g' | wc -c) * 1)) # times 1 to strip whitespace
+if [[ $? != 0 ]]; then
+	echo "** FAILED **"
+	die "Error in csv file." 5
+fi
+
+# Loop through csv and make sure each row has an equal number of commas
+for ((i=1;i<=csvlines;i++)); do
+    cols=$(($(cat $fname | head -$i $fname | sed 's/[^,]//g' | wc -c) - 0))
+	if [[ $(($csvcols * $i)) != $cols ]]; then 
+		echo "** FAILED **"
+		die "Error in csv file." 5
+	fi
+done
+
+echo "** PASSED **"
+((passed++))
+
+# Test Two: Check number of rows in csv and psql table
+echo
+echo "*** Test Two: Number of Rows ***"
+
+# Get rows in postgres table
 count=`psql -A -t -v ON_ERROR_STOP=1 -d ${database} -c "SELECT COUNT(*) from $tablename;"`
 if [[ $? != 0 ]]; then
 	die "Error in psql." 4
 fi
 count=${count[0]}
 
-# Get rows in csv
-csvlines=$(($(cat $fname | wc -l) - 1))
-if [[ $? != 0 ]]; then
-	die "Error in csv file." 5
-fi
-
-
 echo "Rows in table: ${count}"
 echo "Rows in CSV (excluding header row): ${csvlines}"
 
 # Check for equality
 if [ "$count" = "$csvlines" ]; then
-    echo "PASSED"
+    echo "** PASSED **"
     ((passed++))
 else
-    echo "FAILED"
+    echo "** FAILED **"
 fi  
+
+# Test Three: Check number of cols in csv and psql table
+echo
+echo "*** Test Three: Number of Columns ***"
+
+# Get cols in postgres table
+count=`psql -A -t -v ON_ERROR_STOP=1 -d ${database} -c "SELECT COUNT(*) from information_schema.columns
+where table_name='$tablename';"`
+if [[ $? != 0 ]]; then
+	die "Error in psql." 4
+fi
+# Subtract non-key cols
+count=$(($count - $keys))
+
+echo "Non-key columns in table: ${count}"
+echo "Columns in csv: ${csvcols}"
+
+# Check for equality
+if [ "$count" = "$csvcols" ]; then
+    echo "** PASSED **"
+    ((passed++))
+else
+    echo "** FAILED **"
+fi  
+
 
 echo
 echo "Passed ${passed}/${tests} tests."
