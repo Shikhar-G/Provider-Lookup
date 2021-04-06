f19547935dda637ec0bb956430cf482953d86b02
Shikhar Gupta <shikhar.gupta@utexas.edu>
2021-04-06 16:28:49 -0500

    fixed dependencies

--- web/web/settings.py
+++ web/web/settings.py
@@ -61,6 +61,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'web.urls'
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
