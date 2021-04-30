5ee79e1d7001013593c3eab1eeca654e4c6c0d49
Shikhar Gupta <shikhar.gupta@utexas.edu>
2021-04-30 13:03:17 -0500

    refactoring

--- web/provider/templatetags/replace.py
+++ web/provider/templatetags/replace.py
@@ -6,6 +6,7 @@ import phonenumbers
 
 register = template.Library()
 
+
 # from https://stackoverflow.com/questions/44048156/django-filter-use-paginations
 @register.simple_tag
 def relative_url(value, field_name, urlencode=None):
@@ -17,28 +18,31 @@ def relative_url(value, field_name, urlencode=None):
         url = '{}&{}'.format(url, encoded_querystring)
     return url
 
+
 # from https://stackoverflow.com/questions/2272370/sortable-table-columns-in-django
 @register.simple_tag
 def url_replace(request, field, value, direction=''):
     dict_ = request.GET.copy()
 
     if field == 'order_by' and field in dict_.keys():
-      if dict_[field].startswith('-') and dict_[field].lstrip('-') == value:
-        dict_[field] = value
-      elif dict_[field].lstrip('-') == value:
-        dict_[field] = "-" + value
-      else:
-        dict_[field] = direction + value
+        if dict_[field].startswith('-') and dict_[field].lstrip('-') == value:
+            dict_[field] = value
+        elif dict_[field].lstrip('-') == value:
+            dict_[field] = "-" + value
+        else:
+            dict_[field] = direction + value
     else:
-      dict_[field] = direction + value
+        dict_[field] = direction + value
 
     return urlencode(OrderedDict(sorted(dict_.items())))
+
 
 @register.simple_tag
 def replace_addr(value):
     return str(re.sub('[^A-Za-z0-9,]+', '', value))
 
+
 @register.simple_tag
 def replace_phone(value):
     return str(phonenumbers.format_number(phonenumbers.parse(value, 'US'),
-                           phonenumbers.PhoneNumberFormat.NATIONAL))
+                                          phonenumbers.PhoneNumberFormat.NATIONAL))
--- web/web/settings.py
+++ web/web/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tciwyyoi+e)o$((h#-m_e@fg(ynqq^vmzb7l5!13iabo-)b^ty'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
 SITE_URL = "http://127.0.0.1:8000"
 
@@ -76,7 +77,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
             'builtins': [
-                'provider.templatetags.render_url'
+                'provider.templatetags.replace'
             ]
         },
     },
